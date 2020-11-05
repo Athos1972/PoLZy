c0de8fa56f60c173067e4b6717386ee1f4013ba4
BBuhlAir <buhl@buhl-consulting.com.cy>
2020-11-05 08:59:56 +0200

    Partner search next steps

--- backend/setup.py
+++ backend/setup.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name="polzybackend",
-        version="0.0.16",
+        version="0.0.17",
         author="Bernhard Buhl",
         author_email="buhl@buhl-consulting.com.cy",
         description="Open source Insurance Policy Life Cycle Management",
