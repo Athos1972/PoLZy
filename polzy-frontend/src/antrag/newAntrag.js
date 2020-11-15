8d233d2eac6aa2eb04dbfd12183c175097c26c23
aguryev <aguryev@ukr.net>
2020-11-15 15:48:02 +0200

    antrag: restyled product select field

--- polzy-frontend/src/antrag/newAntrag.js
+++ polzy-frontend/src/antrag/newAntrag.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { CardHeader, CardContent, } from '@material-ui/core'
+import { CardHeader, CardContent, TextField, Typography } from '@material-ui/core'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { makeStyles } from '@material-ui/core/styles'
 import { useTranslation } from 'react-i18next'
 import { addAntrag } from '../redux/actions'
 import { getProducts } from '../api'
 import { CardNew, CardLogo } from '../styles/cards'
 import MenuButton from '../components/menuButton'
+import { ProductIcon } from '../components/icons'
 import logo from '../logo/LEZYSEM5-02.png'
+
+// Styles
+const useStyles = makeStyles((theme) => ({
+  inputField: {
+    width: 350,
+  },
+
+  options: {
+    marginLeft: theme.spacing(1),
+  }
+}))
 
 
 function NewAntrag(props) {
 
-  const [productList, setProductList] = useState([])
   const {t, i18n} = useTranslation('antrag')
+  const classes = useStyles()
+
+  const [productList, setProductList] = useState([])
+  const [text, setText] = useState('')
 
   useEffect(() => {
     getProducts(i18n.language, props.user.stage).then((data) => {
@@ -21,18 +38,27 @@ function NewAntrag(props) {
     })
   }, [props.user.stage])
 
-  const handleProductSelect = (value) => {    
+  const handleProductSelect = (event, value) => {
+    console.log('SELECTED:')
+    console.log(value)
+
+    if (value === null) {
+      return
+    }
+
     props.newAntrag({
       request_state: "waiting",
       product_line: {
-        name: value,
+        name: value.description,
       },
       stage: props.user.stage,
     })
+
+    setText('')
   }
 
-  //console.log("ANTRAG PRODUCTS:")
-  //console.log(productList)
+  console.log("ANTRAG PRODUCTS:")
+  console.log(text)
 
   return(
     <CardNew>
@@ -41,11 +67,37 @@ function NewAntrag(props) {
           title={t("antrag:fast.offer")}
         />
         <CardContent>
-          <MenuButton
-            id="product-select"
-            title={t("antrag:select.product")}
-            items={productList}
-            onClick={handleProductSelect}
+          <Autocomplete
+            classes={{root: classes.inputField}}
+            id="antrag-products"
+            options={productList}
+            getOptionSelected={(option, value) => option.description === value.description}
+            getOptionLabel={(option) => option.description}
+            size="small"
+            inputValue={text}
+            value={null}
+            onInputChange={(e,v) => setText(v)}
+            onChange={handleProductSelect}
+            renderInput={(params) => 
+              <TextField 
+                {...params}
+                label={t("antrag:select.product")}
+                variant="outlined"
+              />
+            }
+            renderOption={(option) => {
+              return(
+                <React.Fragment>
+                  <ProductIcon icon={option.name.slice(0,8)} />
+                  <Typography
+                    classes={{root: classes.options}}
+                    variant="body1"
+                  >
+                    {option.description}
+                  </Typography>
+                </React.Fragment>
+              )
+            }}
           />
         </CardContent>
       </div>
