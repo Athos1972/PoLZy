440845b948432385dadcb112043fd1af869b576c
bernhardbuhl <buhl@buhl-consulting.com.cy>
2021-01-17 10:43:51 +0100

    Version bump

--- backend/setup.py
+++ backend/setup.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name="polzybackend",
-        version="0.3.9",
+        version="0.5.0",
         author="Bernhard Buhl",
         author_email="buhl@buhl-consulting.com.cy",
         description="Open source Insurance Policy Life Cycle Management",
