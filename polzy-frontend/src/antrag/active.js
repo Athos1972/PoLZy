00128ed885ab941f38899b21cbe159a1f2018199
aguryev <aguryev@ukr.net>
2021-03-13 19:19:49 +0200

    update activity if it exists

--- polzy-frontend/src/antrag/active.js
+++ polzy-frontend/src/antrag/active.js
@@ -288,8 +288,10 @@ function ActiveAntrag(props) {
     // update activity values
     if (Boolean(currentActivity)) {
       const updatedActivity = antrag.possible_activities.filter(activity => activity.name === currentActivity.name)[0]
-      setActivity(updatedActivity)
-      getActivityValues(updatedActivity)
+      if (updatedActivity) {
+        setActivity(updatedActivity)
+        getActivityValues(updatedActivity)
+      }
     }
   }, [antrag])
 
