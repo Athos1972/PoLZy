8a104864c627148996dc55f2f92a6c619b41da1e
aguryev <aguryev@ukr.net>
2021-06-11 13:26:00 +0300

    documenting: API general

--- polzy-frontend/src/api/general.js
+++ polzy-frontend/src/api/general.js
@@ -1,8 +1,19 @@
-/*
-** General API Calls
-*/
+/**
+ * General back-end API calls
+ *
+ * @category API Calls
+ * @module General
+ */
 
-// get available stages
+
+/**
+ * GET `/api/stages`<br/>
+ * Retrieves a list of the possible stages of PoLZy back-end.
+ *
+ * @async
+ * @function
+ * @returns {array}
+ */
 export const getStages = async () => {
   const response = await fetch('/api/stages')
   const data = await response.json()
@@ -14,7 +25,24 @@ export const getStages = async () => {
   throw new Error(data.error)
 }
 
-// get value list
+/**
+ * POST `/api/values`<br/>
+ * Retrieves a value list, which is a long list of valus for some dropdown fields.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {object} payload
+ * pushed data
+ * @arg {string} payload.instanceId
+ * ID of the instance which calls for the value list
+ * @arg {string} payload.valueListName
+ * name of the value list 
+ * @returns {array}
+ */
 export const getValueList = async (user, payload) => {
   const response = await fetch('/api/values', {
     method: 'POST',
@@ -33,7 +61,31 @@ export const getValueList = async (user, payload) => {
   throw new Error(data.error)
 }
 
-// file handling
+
+/*
+** File Handling
+*/
+
+/**
+ * * POST `/api/upload`
+ * * POST `/api/upload/{parentId}/{fileType}`
+ *
+ * Uploads a file to PoLZy system.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {string} parentId
+ * ID of the instance which uploads the file.
+ * Use _null_ or _undefined_ to upload a file which is not related to a PoLZy instance.
+ * @arg {string} fileType
+ * type of the file to be uploaded
+ * @arg {blob} file
+ * the file to be uploaded
+ */
 export const uploadFiles = async (user, parentId, fileType, file) => {
   // build URI
   const uri = parentId && fileType ? `/api/upload/${parentId}/${encodeURIComponent(fileType)}` : '/api/upload'
@@ -60,6 +112,21 @@ export const uploadFiles = async (user, parentId, fileType, file) => {
   throw new Error(data.error)
 }
 
+/**
+ * GET `/api/documents/{fileId}`<br/>
+ * Retrieves a file from PoLZy system.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {string} fileId
+ * ID of the file to be retrieved
+ * @returns {string}
+ * object URL of the retrieved file
+ */
 export const getFile = async (user, fileId) => {
   // build URI
   const uri = `/api/documents/${fileId}`
@@ -82,6 +149,21 @@ export const getFile = async (user, fileId) => {
   throw new Error(response.statusText)
 }
 
+/**
+ * POST `/api/documents/{fileId}`<br/>
+ * Edit the type of a file within PoLZy system by ID.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {string} fileId
+ * ID of the file to be edited
+ * @arg {string} fileType
+ * a new type of the file
+ */
 export const editFile = async (user, fileId, fileType) => {
   // build URI
   const uri = `/api/documents/${fileId}`
@@ -98,14 +180,24 @@ export const editFile = async (user, fileId, fileType) => {
     })
   })
 
-  //const blob = await response.blob()
-  //const src = await (window.URL ? window.URL : window.webkitURL).createObjectURL(blob)
-
   if (!response.ok) {
     throw new Error(response.statusText)
   }
 }
 
+/**
+ * DELETE `/api/documents/{fileId}`<br/>
+ * Delete a file from PoLZy system by ID.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {string} fileId
+ * ID of the file to be deleted
+ */
 export const deleteFile = async (user, fileId) => {
   // build URI
   const uri = `/api/documents/${fileId}`
@@ -123,6 +215,21 @@ export const deleteFile = async (user, fileId) => {
   } 
 }
 
+/**
+ * GET `/{uri}`<br/>
+ * Retrieves a resource from PoLZy system by URL.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {string} uri
+ * URL of a resource within PoLZy system to be retrieved
+ * @returns {string}
+ * object URL of the retrieved resource
+ */
 export const getResource = async (user, uri) => {
   // call api
   const response = await fetch(uri, {
@@ -142,6 +249,25 @@ export const getResource = async (user, uri) => {
   throw new Error(response.statusText)
 }
 
+/**
+ * POST `/api/remotedocuments`<br/>
+ * Retrieves remote documents from PoLZy system.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {object} payload
+ * request payload
+ * @arg {string} payload.parentId
+ * ID of the instance which requests the documents
+ * @arg {array} payload.documentsId
+ * list of IDs of the requested documents
+ * @returns {string}
+ * object URL of the retrieved document/zipped documents
+ */
 export const getDocuments = async (user, payload) => {
   // call api
   const response = await fetch('/api/remotedocuments', {
@@ -163,6 +289,25 @@ export const getDocuments = async (user, payload) => {
   return src
 }
 
+/**
+ * POST `/api/generateeml`<br/>
+ * Retrieves a *._eml_ file with the remote documents attached.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {object} payload
+ * request payload
+ * @arg {string} payload.parentId
+ * ID of the instance which requests to generate the *._eml_
+ * @arg {array} payload.documentsId
+ * list of IDs of the documents attached to the *._eml_
+ * @returns {string}
+ * object URL of the retrieved *._eml_ file
+ */
 export const generateEml = async (user, payload) => {
   // call api
   const response = await fetch('/api/generateeml', {
@@ -184,6 +329,23 @@ export const generateEml = async (user, payload) => {
   return src
 }
 
+/**
+ * POST `/api/generateantrageml`<br/>
+ * Retrieves a *._eml_ file with the attached summary of the product offer.
+ *
+ * @async
+ * @function
+ * @arg {object} user
+ * the current user's data
+ * @arg {string} user.accessToken
+ * the current user's access token
+ * @arg {object} payload
+ * request payload
+ * @arg {string} payload.parentId
+ * ID of the instance of a product offer which summary should be attached to the *._eml_
+ * @returns {string}
+ * object URL of the retrieved *._eml_ file
+ */
 export const getAntragEmail = async (user, payload) => {
   // call api
   console.log(payload)
